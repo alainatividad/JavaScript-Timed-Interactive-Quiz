507b4a30290599104112663a8f9118f3771ec324
AN <99376802+alainatividad@users.noreply.github.com>
2022-03-13 03:20:00 +1100

    Added code for the highscore page and further enhancements on gameplay

---
+++ Assets/js/script-highscore.js
@@ -0,0 +1,47 @@
+var backButton = document.querySelector(".back-button");
+var resetButton = document.querySelector(".reset-button");
+var listEl = document.querySelector(".highscore-list-container");
+var buttonContainer = document.querySelector(".button-container");
+
+if (backButton !== null) {
+    backButton.addEventListener("click", function () {
+        document.location.href = "./index.html";
+    });
+};
+
+if (resetButton !== null) {
+    resetButton.addEventListener("click", function () {
+        localStorage.setItem("userScores", null);
+
+        // remove ul and li elements
+        var ul = document.querySelector("#highscore-list");
+        listEl.removeChild(ul);
+        // also remove clear highscores
+        buttonContainer.removeChild(resetButton);
+    });
+};
+
+window.onload = loadScores();
+
+function loadScores() {
+    
+    var ul = document.createElement("ul");
+    ul.setAttribute("id", "highscore-list");
+    ul.setAttribute("style", "text-align: left;")
+    listEl.appendChild(ul);
+
+    var userScores = JSON.parse(localStorage.getItem("userScores"));
+    
+    //  read each array and get its name and score
+    if (userScores !== null) {
+        userScores.forEach((element, index) => {
+            li = document.createElement("li");
+            listClass = index + 1;
+            li.setAttribute("class", "li-" + listClass);
+            li.textContent = listClass + ". " + element.name + " - " + element.score;
+            ul.appendChild(li);    
+        });        
+    } else {
+        buttonContainer.removeChild(resetButton);
+    }
+}
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,7 @@ var questionEl = document.querySelector(".text-heading");
 var textEl = document.querySelector(".text-container");
 
 // variables for gameplay
-// questions are from https://www.interviewbit.com/javascript-mcq/
+// questions are from https://www.interviewbit.com/javascript-mcq/ and https://www.interviewbit.com/javascript-interview-questions/
 var questionArray = [
     {
         question: "Which of the following keywords are used to define a variable in JavaScript?",
@@ -31,6 +31,54 @@ var questionArray = [
         choice3: "convert()",
         choice4: "None of the above",
         answer: "1"   
+    },
+    {
+        question: "How do you stop an interval timer in JavaScript?",
+        choice1: "clearInterval",
+        choice2: "clearTimer",
+        choice3: "intervalOver",
+        choice4: "None of the above",
+        answer: "1"   
+    },
+    {
+        question: "How are objects compared when they are checked with the strict equality operator (===)?",
+        choice1: "The contents of the objects are compared",
+        choice2: "Their references are compared",
+        choice3: "Both 1 and 2",
+        choice4: "None of the above",
+        answer: "2"   
+    },
+    {
+        question: "What is the output of typeof NaN?",
+        choice1: "null",
+        choice2: "undefined",
+        choice3: "number",
+        choice4: "string",
+        answer: "3"   
+    },
+    {
+        question: "How can a dataytpe be declared to be a constant type?",
+        choice1: "var",
+        choice2: "let",
+        choice3: "constant",
+        choice4: "const",
+        answer: "4"   
+    },
+    {
+        question: "How are two values compared when they are checked with the strict equality operator (===)?",
+        choice1: "Their values are compared",
+        choice2: "Their datatypes are compared",
+        choice3: "Both 1 and 2",
+        choice4: "None of the above",
+        answer: "3"   
+    },
+    {
+        question: "How do you convert Objects to string using JSON?",
+        choice1: "JSON.parse()",
+        choice2: "JSON.stringify()",
+        choice3: "JSON.string()",
+        choice4: "None of the above",
+        answer: "2"   
     }
 ]
 var gameStatus = false;
@@ -40,25 +88,21 @@ var currentQuestionIndex = 0;
 var currentSortArrayIndex = 0;
 var score = 0;
 
-// Timer that counts down from 10
+// Timer that counts down from 120
 function countdown(timeLeft) {
     if (typeof timeLeft === 'undefined') {
         timeLeft = 120;
     }
   
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > -1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft;
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else {
-        gameStatus = false;
-        clearInterval(timeInterval);
-          gameOver();
-      }
+        if (timeLeft > -1) {
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+        } else {
+            gameStatus = false;
+            clearInterval(timeInterval);
+            gameOver();
+        }
     }, 1000);
 }
 
@@ -76,16 +120,34 @@ function randomSort(arr) {
     }
 }
 
+// "Flashes" the Correct or Incorrect text after answering a question 
+function flashText() {
+    var timeOut = 3;
+    var timeFooter;
+    var footerQuestionEl = document.querySelector(".question-footer");
+
+    timeFooter = setInterval(function () { 
+        if (timeOut > 1) {
+            timeOut--;
+        } else {
+            footerQuestionEl.setAttribute("style", "display: none;"); 
+            clearInterval(timeFooter);
+        }
+    }, 1000);
+}
+
 function initQuestion(currentSortArrayIndex) {
     // remove the current elements inside boxEl except for H1
-    // thendynamically create a list for the button answers
+    // then dynamically create a list for the button answers
     var ul;
     var li;
     var button;
     var buttonOld;
-    currentQuestionIndex = sortArrayQuestions[currentSortArrayIndex];
-
+    var footerQuestionEl;
+    var footerHeader;
+    
     // replace header with question and move choice into a separate array
+    currentQuestionIndex = sortArrayQuestions[currentSortArrayIndex];
     questionEl.textContent = questionArray[currentQuestionIndex].question;
     choiceArray = [
         questionArray[currentQuestionIndex].choice1,
@@ -102,36 +164,44 @@ function initQuestion(currentSortArrayIndex) {
         ul = document.createElement("ul");
         ul.setAttribute("id", "choices");
         boxEl.appendChild(ul);
+
+        // create 'footer' to show if answer is correct or not
+        footerQuestionEl = document.createElement("div");
+        footerQuestionEl.setAttribute("class", "question-footer");
+        footerQuestionEl.setAttribute("style", "display: none;")
+        boxEl.append(footerQuestionEl);
     
+        footerHeader = document.createElement("h2");
+        footerHeader.setAttribute("class", "question-head");
+        footerQuestionEl.append(footerHeader);
+
+        // Create button for every choice
         choiceArray.forEach((element, index) => {
+            listClass = index + 1;
             li = document.createElement("li");
             button = document.createElement("button");
-            listClass = index+1;
-            // li.setAttribute("style","margin: 2px;")
-            li.setAttribute("class", "li-"+listClass);
+            li.setAttribute("class", "li-" + listClass);
+            
             button.textContent = listClass + ". " + element + " ";
             button.setAttribute("class", listClass);
-            button.setAttribute("style", "height: 25px;");  //width: 30vw; 
+            
             ul.appendChild(li);
             li.appendChild(button);
-
         });
     } else {
-        // if next questions are loaded, just replace the current buttons' textContent with the new question's ones
+        // for the next questions, just replace the current buttons' textContent with the new question's ones
         choiceArray.forEach((element, index) => {
             listClass = index + 1;
             li = document.getElementsByClassName("li-"+listClass);
-            button = document.createElement("button");
             buttonOld = document.getElementsByClassName(listClass);
-            // li.setAttribute("style","margin: 2px;")
+            button = document.createElement("button");
             button.textContent = listClass + ". " + element + " ";
             button.setAttribute("class", listClass);
-            button.setAttribute("style", "height: 25px;"); //width: 30vw; 
             li[0].appendChild(button);
-            buttonOld[0].replaceWith(button)
-
+            buttonOld[0].replaceWith(button);
         });
     }
+    
 }
 
 function gameOver() {
@@ -141,7 +211,9 @@ function gameOver() {
     boxEl.removeChild(ul);
 
     // replace header new text
-    questionEl.textContent = "Congratulations! All done!"
+    questionEl.textContent = (score >= Math.round(questionArray.length * 0.70)) ? "Congratulations! All done!" : "Better luck next time! All done!";
+
+    // create new elements
     var div = document.createElement("div");
     div.setAttribute("class", "text-containter");
     boxEl.appendChild(div);
@@ -164,9 +236,43 @@ function gameOver() {
 
     var buttonForm = document.createElement("button");
     buttonForm.setAttribute("class", "submit-button");
-    buttonForm.setAttribute("style", "width: 10vw; height: 25px;");
+    buttonForm.setAttribute("style", "width: 10vw; height: 40px;");
+    buttonForm.addEventListener("click", submitClick);
     buttonForm.textContent = " Submit ";
     labelForm.appendChild(buttonForm);
+}
+
+function submitClick(event) {
+    // prevent refreshing of page when Submit button is clicked
+    event.preventDefault();
+
+    var scoreArray = [];
+    var userScores = JSON.parse(localStorage.getItem("userScores"));
+    
+    var userNewScore = {
+        name: document.querySelector("input").value.trim(),
+        score: score
+    }
+
+    if (userNewScore.name === "") {
+        displayMessage("error", "Initials cannot be blank");
+    }
+
+    // Add new saved initial-score
+    if (userScores === null) {
+        scoreArray.push(userNewScore);
+    } else {
+        for (var i = 0; i < userScores.length; i++) {
+            scoreArray.push(userScores[i]);
+        }
+        scoreArray.push(userNewScore);
+        // sort scores in descending order
+        scoreArray = scoreArray.sort((c1, c2) => (c2.score - c1.score));
+    }
+
+    localStorage.setItem("userScores", JSON.stringify(scoreArray));
+    // change webpage
+    document.location.href = "./highscores.html"
 }
 
 // Start button is pressed
@@ -186,27 +292,37 @@ startButton.addEventListener("click", function () {
 boxEl.addEventListener("click", function (event) {
     var element = event.target;
     var timeLeft;
-
+    var footerQuestionEl = document.querySelector(".question-footer");
+    var footerHeader = document.querySelector(".question-head");
+    
+    footerQuestionEl.setAttribute("style", "font-style: italic;");
+    
     if (element.matches("button")) {
         var chosenAnswer = element.getAttribute("class");
 
         if (chosenAnswer !== "start-button") {
             if (chosenAnswer === questionArray[currentQuestionIndex].answer) {
-                window.alert("Correct!");
+                footerHeader.textContent = "Correct!"
+                // show footer
+                flashText();
                 score++;
                 if (currentSortArrayIndex < questionArray.length-1) {
                     currentSortArrayIndex++;
                     initQuestion(currentSortArrayIndex);
                 } else {
+                    footerQuestionEl.setAttribute("style", "display: none;");  
                     gameStatus = false;
                     gameOver();
                 }
             } else {
                 // Stop current timer, adjust timeLeft, and start countdown again with new timeLeft value
+                footerHeader.textContent = "Incorrect!"
+                flashText();
+
+                timeLeft = timerEl.textContent - 10;
                 clearInterval(timeInterval);
-                timeLeft = timerEl.textContent;
-                timeLeft -= 10;
-                if (timeLeft === 0) {
+                if (timeLeft <= 0) {
+                    footerQuestionEl.setAttribute("style", "display: none;");  
                     gameStatus = false;
                     gameOver();
                 }
@@ -216,21 +332,3 @@ boxEl.addEventListener("click", function (event) {
         }
     }
 })
-
-document.addEventListener('click',function(e){
-    if(e.target.className === 'submit-button'){
-        e.preventDefault();
-
-        var userObject = {
-            name: document.querySelector("input").value.trim(),
-            score: score
-        }
-
-        if (userObject.name === "") {
-            displayMessage("error", "Initials cannot be blank");
-        }
-
-        localStorage.setItem("userObject", JSON.stringify(userObject));
-        document.location = "./highscores.html"
-     }
- });
